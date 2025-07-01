8c3638dd5846e228104b4f9776ad35bcdb4e6507
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-30 22:40:51 -0500

    test: add test to ensure OverflowError is raised on exceeding max size

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,11 +1,20 @@
 from shopping_cart import ShoppingCart
+import pytest
 
 def test_can_add_item_to_cart():
-    cart = ShoppingCart()
+    cart = ShoppingCart(5)
     cart.add("apple")
     assert cart.size() == 1
 
 def test_when_item_added_then_cart_contains_item():
-    cart = ShoppingCart()
+    cart = ShoppingCart(5)
     cart.add("apple")
     assert "apple" in cart.get_items()
+
+def test_when_add_more_than_max_items_should_fail():
+    cart = ShoppingCart(5)
+    for _ in range(5):
+        cart.add("apple")
+    with pytest.raises(OverflowError):
+        cart.add("apple")
+
